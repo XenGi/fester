64fad363ed2e809444dfcfea2946392d71b86b0e
XenGi <me@xengi.de>
2013-11-09 00:37:07 +0100

    main python module added

---
+++ fester.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Fester(object):
+    """
+    The main class
+    """
+    def __init__(self):
+        """
+        Init all the things
+        """
+
+
+if __name__ == '__main__':
+    fester = Fester()
