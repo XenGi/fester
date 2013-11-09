616ea0fb5954b1cf1006547f78268903a325d67e
Uwe Kamper <me@uwekamper.de>
2013-11-09 02:33:13 +0000

    works

--- setup.py
+++ setup.py
@@ -9,5 +9,7 @@ import os
 setup (
     cmdclass = {'build_ext': build_ext } ,
     ext_modules = [Extension ("fester.integrate", ["fester/integrate.pyx", "fester/qeo_types.c", "fester/main2.c", "fester/QGauge_NetStatMessage.c"],
-    extra_compile_args=['-m32', '-g', '-Wall',  '-I%s/c/include' % os.environ['QEO_HOME']])]
+    extra_compile_args=['-g', '-Wall',  '-I%s/c/include' % os.environ['QEO_HOME']],
+    libraries=['rt','qeo', 'qeoutil', 'pthread'],
+    library_dirs=['%s/c/lib/arm-linux-gnueabihf' % os.environ['QEO_HOME']])]
 )
