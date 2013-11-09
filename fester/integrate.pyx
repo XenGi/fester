68068cd596657df629956ed31f98001749aaea59
Mina Nagy Zaki <mnzaki@gmail.com>
2013-11-09 03:15:02 +0200

    Try with distutils

--- fester/integrate.pyx
+++ fester/integrate.pyx
@@ -1,7 +1,3 @@
 # -*- coding: utf-8 -*-
-
-def hallo(a, b):
-    return hallo2(a, b)
-
-cdef double hallo2(double a, double b):
-    return a + b
+cdef extern from "main2.h":
+  int dosomething()
--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import os
 
 setup (
     cmdclass = {'build_ext': build_ext } ,
-    ext_modules = [Extension ("fester.integrate", ["fester/integrate.pyx"]),
-])
+    ext_modules = [Extension ("fester.integrate", ["fester/integrate.pyx", "fester/qeo_types.c", "fester/main2.c", "fester/QGauge_NetStatMessage.c"],
+    extra_compile_args=['-m32', '-g', '-Wall',  '-I%s/c/include' % os.environ['QEO_HOME']])]
+)
