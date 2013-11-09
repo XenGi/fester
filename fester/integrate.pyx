9e3aeec4f805932e84c944be2acb487c940142e5
Uwe Kamper <me@uwekamper.de>
2013-11-09 01:05:43 +0100

    compilen mit cython geht

---
+++ fester/integrate.pyx
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+def hallo(a, b):
+    return hallo2(a, b)
+
+cdef double hallo2(double a, double b):
+    return a + b
---
+++ test_fester.py
@@ -0,0 +1,5 @@
+from fester.integrate import hallo
+
+if __name__ == '__main__':
+    result = hallo(1.0, 2.0)
+    print result
