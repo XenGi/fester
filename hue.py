a19ef1ad96eb0ad7fc700a1b76c8c1b1980cb546
Uwe Kamper <me@uwekamper.de>
2013-11-24 02:56:59 +0100

    lights react to mood messages such as 'sad', 'happy', ...

--- hue.py
+++ hue.py
@@ -5,8 +5,22 @@ import json
 import time
 import random
 
+from fester.integrate import QeoApi
 
 hue = {}
+
+CALM_COLORS = [(13122, 211, 144), ]
+ENERGY_COLORS = [(13122, 211, 253), (13122, 211, 253), (47175, 211, 253)]
+HAPPY_COLORS = [(16352, 45, 237), (60234, 253, 230), (47124, 253, 177)]
+SAD_COLORS = [(51632, 253, 228), (49054, 226, 228), (51934, 253, 288)]
+
+COLORS = {
+    'calm': CALM_COLORS,
+    'energy': ENERGY_COLORS,
+    'happy': HAPPY_COLORS,
+    'sad': SAD_COLORS
+}
+
 hue['calm']   = (13122,13122,13122)
 hue['energy'] = (13122,13122,47175)
 hue['happy']  = (16352,60234,47124)
@@ -24,22 +38,55 @@ sat['energy'] = (211,211,211)
 sat['happy']  = (45,253,253)
 sat['sad']    = (253,226,253)
 
+class Light(object):
 
-def main():
-    r = requests.put("http://10.0.0.247/api/newdeveloper/lights/1/state/", data=json.dumps({'on': True, 'hue': hue['happy'], 'sat': sat, 'bri': bri }))
-    r = requests.put("http://10.0.0.247/api/newdeveloper/lights/2/state/", data=json.dumps({'on': True, 'hue': v, 'sat': sat, 'bri': bri }))
-    r = requests.put("http://10.0.0.247/api/newdeveloper/lights/3/state/", data=json.dumps({'on': True, 'hue': v, 'sat': sat, 'bri': bri }))
-    time.sleep(10)
+    def __init__(self, light_id):
+        self.light_id = light_id
+
+    def make_request(self, data):
+        url = "http://10.0.0.247/api/newdeveloper/lights/%d/state/" % self.light_id
+        r = requests.put(url, data=json.dumps(data))
+
+    def turn_on(self):
+        data = {'on': True}
+        self.make_request(data)
+
+    def turn_off(self):
+        data = {'on': False}
+        self.make_request(data)
+    
+    def change_hsb(self, h, s, b):
+        data = {
+            'hue': h, 
+            'sat': s, 
+            'bri': b
+        }
+        self.make_request(data)
+
+def set_mood(mood):
+    colors = COLORS[mood]
+    lights = [Light(1), Light(2), Light(3)]
+    for idx, light in enumerate(lights):
+        light.turn_on()
+        color_idx = idx % len(colors)
+        light.change_hsb(*colors[color_idx])
+        
+def my_callback(message):
+    if message in COLORS.keys():
+        set_mood(message)
+    else:
+        print "Ignored unknown mood", message
+
+qeo = QeoApi()
 
 if __name__ == '__main__':
-    r = requests.put("http://10.0.0.247/api/newdeveloper/lights/1/state/", data=json.dumps({'on': False}))
-    r = requests.put("http://10.0.0.247/api/newdeveloper/lights/2/state/", data=json.dumps({'on': False}))
-    r = requests.put("http://10.0.0.247/api/newdeveloper/lights/3/state/", data=json.dumps({'on': False}))
-
-    try:
-        main()
-    except KeyboardInterrupt:
-        r = requests.put("http://10.0.0.247/api/newdeveloper/lights/1/state/", data=json.dumps({'on': False}))
-        r = requests.put("http://10.0.0.247/api/newdeveloper/lights/2/state/", data=json.dumps({'on': False}))
-        r = requests.put("http://10.0.0.247/api/newdeveloper/lights/3/state/", data=json.dumps({'on': False}))
-
+    r = qeo.setup()
+    if r == 0:
+        qeo.start_receiving(my_callback)
+        try:
+            while True: time.sleep(1)
+        except KeyboardInterrupt:
+            qeo.stop_receiving()
+            qeo.teardown()
+             
+        
